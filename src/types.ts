841562178c584467bb42d7d4849a8a4d314b9206
Assigal <alex.gabellone@gmail.com>
2026-05-23 23:01:36 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,20 @@
-export {};
+export interface Movimento {
+  id: number;
+  ramo: string;
+  polizza: string;
+  contraente: string;
+  referenteSospesi: string;
+  referenteSospesiId: string;
+  importo: number;
+  sconto: number;
+  netto: number;
+  modalita: string;
+  tipo: string;
+  sub: string;
+  dataAssegno: string;
+  segno: number;
+  note: string;
+  dataInizioSubagente: string;
+  dataFineSubagente: string;
+  allocazioniRecupero: AllocazioneRecupero[];
+}
