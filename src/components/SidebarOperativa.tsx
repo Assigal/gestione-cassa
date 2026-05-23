9891d5717215145fc4fb73229363f1a854190a6d
Assigal <alex.gabellone@gmail.com>
2026-05-23 22:35:12 +0200

    Update SidebarOperativa.tsx

--- src/components/SidebarOperativa.tsx
+++ src/components/SidebarOperativa.tsx
@@ -12,8 +12,70 @@ import {
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
-type SidebarOperativaProps = {};
+type SidebarOperativaProps = {
+  giornataCorrente: string;
+  setGiornataCorrente: React.Dispatch<React.SetStateAction<string>>;
 
-export function SidebarOperativa({}: SidebarOperativaProps) {
+  session: any;
+  profiloUtente: any;
+
+  giornataChiusa: boolean;
+  giornataDbId: string | null;
+  isAdmin: boolean;
+
+  logout: () => void;
+  chiudiGiornata: () => void;
+  riapriGiornata: () => void;
+
+  versamento: string;
+  aggiornaVersamento: (value: string) => void;
+
+  totals: any;
+  avanzoPrecedente: number;
+
+  quadMezza: { cassaReale: string };
+  setQuadMezza: React.Dispatch<React.SetStateAction<{ cassaReale: string }>>;
+  quadSera: { cassaReale: string };
+  setQuadSera: React.Dispatch<React.SetStateAction<{ cassaReale: string }>>;
+
+  quadMezzaBloccata: any;
+  quadSeraBloccata: any;
+
+  bloccaQuadraturaMezza: () => void;
+  bloccaQuadraturaSera: () => void;
+
+  euro: (value: number) => string;
+  deltaLabel: (value: number) => string;
+
+  SidebarMetric: any;
+};
+
+export function SidebarOperativa({
+  giornataCorrente,
+  setGiornataCorrente,
+  session,
+  profiloUtente,
+  giornataChiusa,
+  giornataDbId,
+  isAdmin,
+  logout,
+  chiudiGiornata,
+  riapriGiornata,
+  versamento,
+  aggiornaVersamento,
+  totals,
+  avanzoPrecedente,
+  quadMezza,
+  setQuadMezza,
+  quadSera,
+  setQuadSera,
+  quadMezzaBloccata,
+  quadSeraBloccata,
+  bloccaQuadraturaMezza,
+  bloccaQuadraturaSera,
+  euro,
+  deltaLabel,
+  SidebarMetric,
+}: SidebarOperativaProps) {
   return null;
 }
