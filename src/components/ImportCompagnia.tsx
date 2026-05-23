4ee9dfd6587ff46849727d49c6f0b3dd20a0fa3b
Assigal <alex.gabellone@gmail.com>
2026-05-23 19:49:47 +0200

    Create ImportCompagnia.tsx

---
+++ src/components/ImportCompagnia.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Upload, ArrowRightCircle, Trash2 } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export function ImportCompagnia() {
+  return null;
+}
