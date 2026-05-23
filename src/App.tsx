f2f0d95d3ed8e98db891509be14a1615347a1d2f
Assigal <alex.gabellone@gmail.com>
2026-05-23 23:00:03 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -50,26 +50,6 @@ interface AllocazioneRecupero {
   sconto: number;
 }
 
-interface Movimento {
-  id: number;
-  ramo: string;
-  polizza: string;
-  contraente: string;
-  referenteSospesi: string;
-  referenteSospesiId: string;
-  importo: number;
-  sconto: number;
-  netto: number;
-  modalita: string;
-  tipo: string;
-  sub: string;
-  dataAssegno: string;
-  segno: number;
-  note: string;
-  dataInizioSubagente: string;
-  dataFineSubagente: string;
-  allocazioniRecupero: AllocazioneRecupero[];
-}
 
 interface Sospeso {
   id: string;
