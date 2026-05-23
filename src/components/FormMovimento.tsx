6e7a4f84db7685b8743762792c6dd1a1b221167c
Assigal <alex.gabellone@gmail.com>
2026-05-23 18:50:59 +0200

    Update FormMovimento.tsx

--- src/components/FormMovimento.tsx
+++ src/components/FormMovimento.tsx
@@ -1,5 +1,37 @@
 import React from "react";
 
-export function FormMovimento() {
-  return null;
+type FormMovimentoProps = {
+  editingMovement: number | null;
+  setEditingMovement: React.Dispatch<React.SetStateAction<number | null>>;
+
+  giornataChiusa: boolean;
+  selectedImportRow: any;
+
+  form: FormState;
+  setForm: React.Dispatch<React.SetStateAction<FormState>>;
+  saveForm: () => void;
+  resetForm: () => void;
+  fileInputRef: React.RefObject<HTMLInputElement>;
+  handleImportFile: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  openImportFileDialog: () => void;
+  handleTipoMovimentoChange: (tipo: string) => void;
+  
+  tipiMovimento: string[];
+  modalitaPagamento: any[];
+  referentiSospesi: any[];
+  
+  getDescrizioneModalita: (codice: string | null | undefined) => string;
+  isVersamentoSubagente: (tipo: string) => boolean;
+  euro: (value: number) => string;
+  
+  giornataCorrente: string;
+  selectedSospesoIds: string[];
+};
+
+export function FormMovimento(props: FormMovimentoProps) {
+  return (
+    <div className="hidden">
+      FormMovimento collegato
+    </div>
+  );
 }
