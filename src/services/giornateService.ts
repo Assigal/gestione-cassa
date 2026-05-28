b0ff3a34ce1dc79f4e462f42845af9bc0b3ee069
Assigal <alex.gabellone@gmail.com>
2026-05-28 11:13:17 +0200

    Update giornateService.ts

--- src/services/giornateService.ts
+++ src/services/giornateService.ts
@@ -1,3 +1,16 @@
 import { supabase } from "../supabaseClient";
 
-export {};
+export async function chiudiGiornataDb(
+  giornataDbId: string,
+  totals: any
+) {
+  return await supabase
+    .from("giornate_cassa")
+    .update({
+      stato: "chiusa",
+      cassa_finale_teorica: totals.cassa,
+      versamento: totals.versamento,
+      chiusa_il: new Date().toISOString(),
+    })
+    .eq("id", giornataDbId);
+}
