542ce1e95626e78558e3aeea73ff8a59f3603b22
Assigal <alex.gabellone@gmail.com>
2026-05-29 11:33:08 +0200

    Update sospesiService.ts

--- src/services/sospesiService.ts
+++ src/services/sospesiService.ts
@@ -1,3 +1,8 @@
 import { supabase } from "../supabaseClient";
 
-export {};
+export async function caricaSospesiDb() {
+  return await supabase
+    .from("sospesi_cassa")
+    .select("*")
+    .order("created_at", { ascending: false });
+}
