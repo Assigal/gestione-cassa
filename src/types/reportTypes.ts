89d18fe91ffa1d1709bf57181618308adc118282
Assigal <alex.gabellone@gmail.com>
2026-06-17 16:31:49 +0200

    Create reportTypes.ts

---
+++ src/types/reportTypes.ts
@@ -0,0 +1,77 @@
+export type ReportPaymentType =
+  | "contanti"
+  | "assegno"
+  | "assegno_postdatato"
+  | "pos"
+  | "bonifico"
+  | "sospeso"
+  | "misto"
+  | "altro";
+
+export interface ReportMovimento {
+  id: string;
+  ora: string;
+  contraente: string;
+  polizza: string;
+  tipoPagamento: string;
+  importo: number;
+  sconto: number;
+  utente: string;
+  cip: string;
+  isPostdatato?: boolean;
+}
+
+export interface ReportRecuperoSospeso extends ReportMovimento {
+  dataOrigineSospeso: string;
+}
+
+export interface ReportChiusuraSubagente {
+  id: string;
+  cip: string;
+  subagente?: string;
+  periodoDal: string;
+  periodoAl: string;
+  importo: number;
+  utente: string;
+  note?: string;
+}
+
+export interface CassaGiornataReport {
+  quadraturaCompagnia: {
+    totaleTitoliCip100: number;
+    totaleCompagnia?: number;
+    differenza?: number;
+    isQuadrata?: boolean;
+  };
+
+  indicatori: {
+    totaleSconti: number;
+    totaleSospesiCreati: number;
+    totaleRecuperiSospesi: number;
+    numeroSospesiCreati: number;
+    numeroRecuperiSospesi: number;
+    numeroAltriCip: number;
+    numeroPostdatati: number;
+  };
+
+  sezioni: {
+    titoliCip100: ReportMovimento[];
+    titoliAltriCip: ReportMovimento[];
+    recuperiSospesi: ReportRecuperoSospeso[];
+    chiusureSubagenti: ReportChiusuraSubagente[];
+    altriMovimenti: ReportMovimento[];
+  };
+
+  cassa: {
+    cassaFisicaIniziale: number;
+    totaleContanti: number;
+    totaleAssegni: number;
+    totalePos: number;
+    totaleBonifici: number;
+    totaleAltriPagamenti: number;
+    versamento: number;
+    cassaFisicaSerale: number;
+  };
+
+  alert: string[];
+}
