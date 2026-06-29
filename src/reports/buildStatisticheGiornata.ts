49f76dc1615f3abfad36ed3a1467db6628f79361
Assigal <alex.gabellone@gmail.com>
2026-06-29 12:42:01 +0200

    Create buildStatisticheGiornata.ts

---
+++ src/reports/buildStatisticheGiornata.ts
@@ -0,0 +1,19 @@
+import type {
+  Movimento,
+  Sospeso,
+} from "../types";
+
+export interface StatisticheGiornata {
+
+}
+
+export function buildStatisticheGiornata(
+  movimenti: Movimento[],
+  sospesi: Sospeso[]
+): StatisticheGiornata {
+
+  return {
+
+  };
+
+}
